4578050fe3f897452b73bb0e10a08705ca71f26e
bacon <googol.zhang@gmail.com>
2017-04-05 16:14:42 -0400

    update accordingly

--- WhereQRealtor.ca/src/main/java/com/whereq/realtor/xml/bind/TestListingMarshing.java
+++ WhereQRealtor.ca/src/main/java/com/whereq/realtor/xml/bind/TestListingMarshing.java
@@ -8,7 +8,7 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
-import com.whereq.realtor.batch.persist.ListingPO;
+import com.whereq.realtor.batch.domain.ListingPO;
 
 public class TestListingMarshing 
 {
